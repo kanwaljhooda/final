f868424dec5a26e7286bca063d15018c68a6dc2d
K.J. Hooda <kj.hooda@kellogg.northwestern.edu>
2021-05-25 14:08:38 -0500

    Built sign-up, sign-in and sign-out functionality

--- index.js
+++ index.js
@@ -2,6 +2,24 @@ firebase.auth().onAuthStateChanged(async function(user) {
   if (user) {
     // Signed in
     console.log('signed in')
+
+    // Display Sign-out option
+    document.querySelector(`.sign-in-or-sign-out`).innerHTML = `
+      <button class="text-pink-500 underline sign-out">Sign Out</button>`
+
+    // Get a reference to the sign out button
+    let signOutButton = document.querySelector(`.sign-out`)
+
+    //  Handle the sign out button 
+    signOutButton.addEventListener(`click`, function(event) {
+      
+      // Sign out of firebase authentication
+      firebase.auth().signOut()
+
+      // Redirect to the home page
+      document.location.href = `index.html`
+    })
+
   } else {
     // Signed out
     console.log('signed out')
--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,14 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+  apiKey: "AIzaSyCrMqLJPhFrZYTPF8V4cBVCOwkA7E4AxB8",
+  authDomain: "final-project---touchpoint.firebaseapp.com",
+  projectId: "final-project---touchpoint",
+  storageBucket: "final-project---touchpoint.appspot.com",
+  messagingSenderId: "709329410311",
+  appId: "1:709329410311:web:d3b6474ff33cba2c92cda2"
+} // replace
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
