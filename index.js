663f9ea7aa9f27df7bd31eb2d923526dd73c9c0c
K.J. Hooda <kj.hooda@kellogg.northwestern.edu>
2021-05-25 14:58:57 -0500

    Main option setup

--- index.js
+++ index.js
@@ -3,22 +3,60 @@ firebase.auth().onAuthStateChanged(async function(user) {
     // Signed in
     console.log('signed in')
 
-    // Display Sign-out option
-    document.querySelector(`.sign-in-or-sign-out`).innerHTML = `
-      <button class="text-pink-500 underline sign-out">Sign Out</button>`
+    // CODE FOR SIGNING OUT
 
-    // Get a reference to the sign out button
-    let signOutButton = document.querySelector(`.sign-out`)
+        // Display Sign-out option
+        document.querySelector(`.sign-in-or-sign-out`).innerHTML = `
+          <button class="text-pink-500 underline sign-out">Sign Out</button>`
 
-    //  Handle the sign out button 
-    signOutButton.addEventListener(`click`, function(event) {
-      
-      // Sign out of firebase authentication
-      firebase.auth().signOut()
+        // Get a reference to the sign out button
+        let signOutButton = document.querySelector(`.sign-out`)
 
-      // Redirect to the home page
-      document.location.href = `index.html`
-    })
+        //  Handle the sign out button 
+        signOutButton.addEventListener(`click`, function(event) {
+          
+          // Sign out of firebase authentication
+          firebase.auth().signOut()
+
+          // Redirect to the home page
+          document.location.href = `index.html`
+        })
+
+    // CODE FOR SIGNING OUT
+
+    // CODE FOR ROUTING TO 2 USER OPTIONS
+    
+        // Display the two main options
+
+        document.querySelector(`.main-options`).innerHTML = `
+          <button class="text-pink-500 underline touchpoint-tracker">Track Touchpoints</button>
+          
+          <button class="text-pink-500 underline contact-manager">Manage Contacts</button>
+          `
+        
+        // Get a reference for the Touchpoint Tracker button
+        let touchpointTrackerButton = document.querySelector(`.touchpoint-tracker`)
+
+
+        // Handle the Touchpoint Tracker button
+        touchpointTrackerButton.addEventListener(`click`, async function(event){
+
+          // Redirect to the tracker page
+          document.location.href = `tracker.html`
+
+        })
+        
+        // Get a reference for the Contact Manager button
+        let contactManagerButton = document.querySelector(`.contact-manager`)
+
+        // Handle the Touchpoint Tracker button
+
+        contactManagerButton.addEventListener(`click`, async function(event){
+
+          // Redirect to the tracker page
+          document.location.href = `contact.html`
+
+        })
 
   } else {
     // Signed out
