1045be3e711db1a38540ea1267f72186405246b9
K.J. Hooda <kj.hooda@kellogg.northwestern.edu>
2021-05-25 20:27:12 -0500

    Add contact lambda function complete

--- add-contact.js
+++ add-contact.js
@@ -26,35 +26,63 @@ firebase.auth().onAuthStateChanged(async function(user) {
   
       // UPDATE LANDING PAGE
 
-      document.querySelector(`.landing-page`).innerHTML = `
-      <form class="items-center">
-        <label class="block mt-4 font-semibold" for="name">Contact Name</label>
-        <input class="p-2 mt-2 w-96 border border-gray-400 rounded focus:outline-none focus:ring-green-700 focus:border-purple-500" type="text" id="name" name="name">
+        document.querySelector(`.landing-page`).innerHTML = `
+        <form class="items-center">
+            <label class="block mt-4 font-semibold" for="name">Contact Name</label>
+            <input class="p-2 mt-2 w-96 border border-gray-400 rounded focus:outline-none focus:ring-green-700 focus:border-purple-500" type="text" id="name" name="name">
 
-        <label class="block mt-4 font-semibold" for="jamatkhana">Email</label>
-        <input class="p-2 mt-2 w-96 border border-gray-400 rounded focus:outline-none focus:ring-green-700 focus:border-purple-500" type="email" id="email" name="email">
+            <label class="block mt-4 font-semibold" for="jamatkhana">Email</label>
+            <input class="p-2 mt-2 w-96 border border-gray-400 rounded focus:outline-none focus:ring-green-700 focus:border-purple-500" type="email" id="email" name="email">
 
-        <label class="block mt-4 font-semibold" for="commit">Phone Number</label>
-        <input class="p-2 mt-2 w-96 border border-gray-400 rounded focus:outline-none focus:ring-green-700 focus:border-purple-500" type="tel" id="phone" name="phone">
+            <label class="block mt-4 font-semibold" for="commit">Phone Number</label>
+            <input class="p-2 mt-2 w-96 border border-gray-400 rounded focus:outline-none focus:ring-green-700 focus:border-purple-500" type="tel" id="phone" name="phone">
 
-        <label class="block mt-4 font-semibold" for="commit">Birthday</label>
-        <input class="p-2 mt-2 w-96 border border-gray-400 rounded focus:outline-none focus:ring-green-700 focus:border-purple-500" type="date" id="birthday" name="birthday">
-        
-        <label class="block mt-4 font-semibold" for="commit">Touchpoint Frequency</label>
-        <select class="p-2 mt-2 w-96 border border-gray-400 rounded focus:outline-none focus:ring-green-700 focus:border-purple-500" type="text" id="frequency" name="frequency">
-            <option value="weekly">Weekly</option>
-            <option value="monthly">Monthly</option>
-            <option value="quarterly">Quarterly</option>
-            <option value="annually">Annually</option>
-        </select>
+            <label class="block mt-4 font-semibold" for="commit">Birthday</label>
+            <input class="p-2 mt-2 w-96 border border-gray-400 rounded focus:outline-none focus:ring-green-700 focus:border-purple-500" type="date" id="birthday" name="birthday">
+            
+            <label class="block mt-4 font-semibold" for="commit">Touchpoint Frequency</label>
+            <select class="p-2 mt-2 w-96 border border-gray-400 rounded focus:outline-none focus:ring-green-700 focus:border-purple-500" type="text" id="frequency" name="frequency">
+                <option value="weekly">Weekly</option>
+                <option value="monthly">Monthly</option>
+                <option value="quarterly">Quarterly</option>
+                <option value="annually">Annually</option>
+            </select>
 
-        <label class="block mt-4 font-semibold" for="commit">Notes</label>
-        <input class="p-2 mt-2 w-96 h-52 border border-gray-400 rounded focus:outline-none focus:ring-green-700 focus:border-purple-500" type="text" id="notes" name="notes">
+            <label class="block mt-4 font-semibold" for="commit">Notes</label>
+            <input class="p-2 mt-2 w-96 h-52 border border-gray-400 rounded focus:outline-none focus:ring-green-700 focus:border-purple-500" type="text" id="notes" name="notes">
 
-        <button class="block mt-4 text-white bg-green-700 rounded px-4 py-2 add-contact">Add Contact</button>
-      </form>`
+            <button class="block mt-4 text-white bg-green-700 rounded px-4 py-2 add-contact">Add Contact</button>
+        </form>`
 
       // UPDATE LANDING PAGE
+
+      // PROCESS CONTACT CREATION VIA API
+
+        // Get a reference to the Add Contact button
+        let addContactButton = document.querySelector(`.add-contact`)
+        
+        // Add event listener to button
+        addContactButton.addEventListener(`click`, async function(event){
+
+            // Ignore default behavior
+            event.preventDefault()
+
+            // Get references to form inputs
+            let name = document.querySelector(`#name`).value
+            let email = document.querySelector(`#email`).value
+            let phone = document.querySelector(`#phone`).value
+            let birthday = document.querySelector(`#birthday`).value
+            let frequency = document.querySelector(`#frequency`).value
+            let notes = document.querySelector(`#notes`).value
+
+            // Build url for contacts API
+            let url = `/.netlify/functions/create_contact?userId=${user.uid}&name=${name}&email=${email}&phone=${phone}&birthday=${birthday}&frequency=${frequency}&notes=${notes}`
+
+            // Fetch the url, wait for a response, store the response in memory
+            let response = await fetch(url)
+            
+        })
+      // PROCESS CONTACT CREATION VIA LAMBDA FUNCTION
 
   
     } else {
---
+++ netlify/functions/create_contact.js
@@ -0,0 +1,38 @@
+// Allow use of firebase
+let firebase = require(`./firebase`)
+
+// For reference: /.netlify/functions/create_contact?userId=${user.uid}&name=${name}&email=${email}&phone=${phone}&birthday=${birthday}&frequency=${frequency}&notes=${notes}
+
+exports.handler = async function(event) {
+
+    // Get the querystring parameters and store in memory
+    let userId = event.queryStringParameters.userId
+    let name = event.queryStringParameters.name
+    let email = event.queryStringParameters.email
+    let phone = event.queryStringParameters.phone
+    let birthday = event.queryStringParameters.birthday
+    let frequency = event.queryStringParameters.frequency
+    let notes = event.queryStringParameters.notes
+
+    // Establish a connection to firebase in memory
+    let db = firebase.firestore()
+    
+    // Create a new contact, wait for it to return
+
+    await db.collection(`contacts`).add({
+        created: firebase.firestore.FieldValue.serverTimestamp(),
+        userId: userId,
+        name: name,
+        email: email,
+        phone: phone,
+        birthday: birthday,
+        touchpointFrequency: frequency,
+        notes: notes
+    })
+
+    return {
+        statusCode: 200,
+        // 🔥 NOTE TO CONNOR: Can't figure out why the string below won't display at the end :/ 🔥
+        body: `Contact, ${name}, has been added!`
+    }
+}
