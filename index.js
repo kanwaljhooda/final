5e559b56201ac0096db30e676dca7ec9a3fbf63d
K.J. Hooda <kj.hooda@kellogg.northwestern.edu>
2021-06-01 08:01:09 -0500

    Upcoming contact logic complete

--- index.js
+++ index.js
@@ -37,20 +37,21 @@ firebase.auth().onAuthStateChanged(async function(user) {
         <button id = "add-contact-${user.uid}" class="block mt-4 text-center text-white bg-green-700 rounded px-4 py-2 ">Add Contact</button>
         `)
 
+      // Get reference to add-contact button
+      let addContactButton = document.querySelector(`#add-contact-${user.uid}`)
+
+      // Handle the button
+      addContactButton.addEventListener(`click`, async function(event){
+
+        // Redirect to the add contact HTML
+        document.location.href = `add-contact.html`
+
+      })
+      
       // END UPDATE LANDING PAGE TITLE
 
       // START FIND AND DISPLAY LAGGING TOUCHPOINTS
-
-          // Get reference to add-contact button
-          let addContactButton = document.querySelector(`#add-contact-${user.uid}`)
-
-              // Handle the button
-              addContactButton.addEventListener(`click`, async function(event){
-
-                // Redirect to the add contact HTML
-                document.location.href = `add-contact.html`
-
-              })
+         
       
           // Build url for API for finding lagging touchpoints
           let laggingUrl = `/.netlify/functions/find_lagging?userId=${user.uid}`
@@ -172,10 +173,118 @@ firebase.auth().onAuthStateChanged(async function(user) {
       // START FIND UPCOMING TOUCHPOINTS
 
           // Build url for API for finding lagging touchpoints
-          // let upcomingUrl = `/.netlify/functions/find_upcoming?userId=${user.uid}`
+          let upcomingUrl = `/.netlify/functions/find_upcoming?userId=${user.uid}`
 
           // Fetch the url, wait for a response, store the response in memory
-          // let upcomingTouchpoints = await fetch(upcomingUrl)
+          let upcomingTouchpoints = await fetch(upcomingUrl)
+          let upcomingContacts = await upcomingTouchpoints.json()
+          // console.log(upcomingContacts)
+
+          // Loop through the lagging contacts to display them
+          for (let uc=0; uc < upcomingContacts.length; uc++){
+
+            let upcomingContact = upcomingContacts[uc]
+            console.log(upcomingContact)
+
+            // Create element for landing page
+            let element = document.querySelector(`.upcoming-contacts`)
+
+            element.insertAdjacentHTML(`beforeend`,`
+            <h1> Upcoming Contacts </h1>
+            <div class=" p-2 m-8 flex border-2 border-black rounded">
+              <div class="p-4 bg-gray-200 rounded w-1/2 text-left">
+                <h1 class="rounded-xl underline text-2xl bg-clip-text text-left">
+                  <span>${upcomingContact.name}</span>
+                </h1>  
+                <div class="flex">
+                  <div class="w-1/2">
+                    <h2 class="text-2xl py-1">📧 ${upcomingContact.email}  ☎️${upcomingContact.phone}</h2>
+                    <p class="font-bold text-gray-600">🎂 ${upcomingContact.birthday}</p>
+                    <p class="font-bold text-gray-600">⏲️ ${upcomingContact.frequency}</p>
+                  </div>
+                </div>
+                <div class="mt-4 flex">
+                  <div>
+                    <div class="text-sm font-bold text-gray-600">NOTES</div>
+                      <p>📝 ${upcomingContact.notes}</p>      
+                    </div>
+                  </div>
+              </div>
+              <div class="m-4 w-1/2 text-right">
+                  <h1 class="rounded-xl underline text-2xl bg-clip-text text-left">
+                    <span>Actions:</span>
+                  </h1>
+                  <button id = "add-touchpoint-${upcomingContact.contactId}" class="block mt-4 text-white bg-green-400 rounded px-4 py-2 ">Mark Complete</button>
+                  <button id = "snooze-${upcomingContact.contactId}" class="block mt-4 text-white bg-yellow-400 rounded px-4 py-2 ">Snooze</button>
+                  <button id = "retire-${upcomingContact.contactId}" class="block mt-4 text-white bg-red-400 rounded px-4 py-2 ">Stop Tracking</button>
+              </div>
+            </div>
+            `
+            )
+
+            // Get a reference to the add touchpoint button
+            let addTouchpoint = document.querySelector(`#add-touchpoint-${upcomingContact.contactId}`)
+
+                //  Handle the button 
+                addTouchpoint.addEventListener(`click`, function(event) {
+                
+                // Prevent default
+                event.preventDefault()
+
+                // Redirect to the HTML
+                document.location.href = `add-touchpoint.html?contactId=${upcomingContact.contactId}&userId=${user.uid}`
+
+            })
+
+            // Get a reference to the snooze button
+            let snoozeContact = document.querySelector(`#snooze-${upcomingContact.contactId}`)
+
+            // console.log(snoozeContact)
+
+                // Handle the button
+                snoozeContact.addEventListener(`click`, async function(event){
+                              
+                // Prevent default
+                event.preventDefault()
+
+                console.log(`${upcomingContact.contactId}`)
+
+                // Build URL for retire API
+                let snoozeContactUrl = `/.netlify/functions/snooze_contact?contactId=${upcomingContact.contactId}`
+
+                // Run API to deactivate contact
+                let snoozeResponse = await fetch(snoozeContactUrl)
+
+                // Refresh the page
+                location.reload()
+
+                })
+            
+            // Get a reference to the stop tracking button
+            let retireContact = document.querySelector(`#retire-${upcomingContact.contactId}`)
+
+            // console.log(retireContact)
+
+                // Handle the button
+                retireContact.addEventListener(`click`, async function(event){
+
+                  // Prevent default
+                  event.preventDefault()
+
+                  // Build URL for retire API
+                  let retireContactUrl = `/.netlify/functions/retire_contact?contactId=${upcomingContact.contactId}`
+
+                  // Run API to deactivate contact
+                  let retireResponse = await fetch(retireContactUrl)
+
+                  // Refresh the page
+                  location.reload()
+
+                })
+
+            
+
+          }       
 
       // END FIND UPCOMING TOUCHPOINTS
       
---
+++ netlify/functions/find_upcoming.js
@@ -0,0 +1,82 @@
+// Allow use of firebase
+let firebase = require(`./firebase`)
+
+// For reference: /.netlify/functions/retrieve_contacts?userId=${user.uid}`
+
+exports.handler = async function(event) {
+
+    // Get the querystring parameters and store in memory
+    let userId = event.queryStringParameters.userId
+    // console.log(userId)
+
+    // Establish a connection to firebase in memory
+    let db = firebase.firestore()
+
+    // Set a new array to capture return value
+    let returnValue = []
+    
+    // Retrieve all contacts, wait for it to return
+    let contactsQuery = await db.collection(`contacts`)
+            .where(`userId`, `==`, userId)
+            .where(`active`,`==`, true)
+            // .orderBy(`name`) ---> TRIED TO ORDER CONTACTS BY NAME BUT DIDN'T WORK
+            .get()
+
+    let contacts = contactsQuery.docs
+    // console.log(contacts)
+
+    let today = new Date()
+    today = today.toLocaleString()
+    console.log(today)
+
+    for (let i=0; i < contacts.length; i++) {
+        
+        // let contactId = contacts[i].id
+        let contact = contacts[i].data()
+
+        let contactId = contacts[i].id
+        let name = contact.name
+        let email = contact.email
+        let phone = contact.phone
+        let birthday = contact.birthday
+        let frequency = contact.frequency
+        let notes = contact.notes
+        let active = contact.active
+        let upcomingTouchpoint = contact.upcomingTouchpoint
+
+        // console.log(upcomingTouchpoint)
+        upcomingTouchpointInDateObject = new Date(upcomingTouchpoint)
+        // console.log(upcomingTouchpointInDateObject)
+        todayAsDateObject = new Date()
+        // console.log(todayAsDateObject)
+        console.log((upcomingTouchpointInDateObject - todayAsDateObject) / (1000 * 60 * 60 * 24))
+
+        dayDifference = (upcomingTouchpointInDateObject - todayAsDateObject) / (1000 * 60 * 60 * 24)
+
+        if (dayDifference <= 15 && dayDifference > 0) {
+
+            // Create contact object
+            let contactObject = {
+                contactId: contactId,
+                name : name,
+                email : email,
+                phone : phone,
+                birthday : birthday,
+                frequency : frequency,
+                notes : notes,
+                active : active
+            }
+
+            // Add contact to return value
+            returnValue.push(contactObject)
+
+        }
+    }
+
+    // Return the standard response
+    return {
+    statusCode: 200,
+    body: JSON.stringify(returnValue)
+    }
+
+}
